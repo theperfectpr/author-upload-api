40dcb53e50aced7963976e929aa147b56d0cfc66
theperfectpr <kbarua.dev@gmail.com>
2024-06-17 19:11:11 +0530

    create data, jwt integration

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,19 +1,42 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModel from "./userModel";
+import bcrypt from "bcrypt";
+import { sign } from "jsonwebtoken";
+import { config } from "../config/config";
 
 const createUser = async (req: Request,res: Response,next: NextFunction) =>{
 
     const {name, email, password} = req.body;
+    console.log(req.body);
+    
     //validation
     if(!name || !email || !password){
         const error = createHttpError(400, "Fields are required");
         return next(error);
     }
+    //database call
+    const user = await userModel.findOne({email});
+    if(user){
+        const error = createHttpError(400,"User already exists with this email");
+        return next(error);
+    }
+    const passwordHash = await bcrypt.hash(password, 10);
+    const newUser = await userModel.create({
+        name,
+        email,
+        password: passwordHash
+    });
+
+    //token generation with JWT
+    const token = sign({sub: newUser._id}, config.jwtSecret as string,{expiresIn: "1d"});
+
+
     //process
 
     //response
 
-    res.json({ message: 'user created' });
+    res.json({accessToken: token});
 }
 
 //module export
---
+++ src/user/userModel.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+import { User } from "./userType";
+
+const userSchema = new mongoose.Schema<User>({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    password:{
+        type: String,
+        required: true
+    }
+},{timestamps: true});
+
+export default mongoose.model<User>("User", userSchema);
+
