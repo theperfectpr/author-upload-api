c9bb43c6271ffa715066a7641643aa4472f03609
theperfectpr <kbarua.dev@gmail.com>
2024-06-17 14:52:58 +0530

    setup express

--- server.ts
+++ server.ts
@@ -1,1 +1,8 @@
-console.log('Welcome to ebook api');
+import app from './src/app';
+
+const startServer = () =>{
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`Server is running on port ${port}`));
+}
+
+startServer();
