5bf921e434d998a288550b80398ee08e63eec02b
theperfectpr <kbarua.dev@gmail.com>
2024-06-18 17:29:09 +0530

    add jwt authorization

--- src/book/bookRouter.ts
+++ src/book/bookRouter.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import { createBook } from './bookController';
 import multer from 'multer';
 import path from 'node:path';
+import authenticate from '../middlewares/authenticate';
 
 
 const bookRouter = express.Router();
@@ -12,8 +13,9 @@ const upload = multer({
     }
 })
 
+
 //do not call this route directly, router will call on request
 //route, middleware->function, handler
-bookRouter.post('/',upload.fields([{name: 'coverImage', maxCount:1},{name: 'file', maxCount:1}]), createBook);
+bookRouter.post('/',authenticate,upload.fields([{name: 'coverImage', maxCount:1},{name: 'file', maxCount:1}]), createBook);
 
 export default bookRouter;
